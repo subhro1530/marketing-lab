b09eb5136f16e43b0e422cc62c94e225e4990905
subhro1530 <shaswata.ssaha@gmail.com>
2024-02-08 08:24:21 +0530

    4:1

--- components/Partners.js
+++ components/Partners.js
@@ -43,64 +43,68 @@ const Partners = () => {
   });
 
   return (
-    <animated.div style={{ opacity }}>
-      <Box
-        width="100vw"
-        overflowX="hidden"
-        bg="#f0eeeb"
-        p={{ base: 4, md: 8 }}
-        height={{ base: "40vh", md: "auto" }}
-        textAlign="center"
-      >
-        <Heading
-          mt={5}
-          fontSize={{ base: "xl", md: "3xl" }}
-          fontWeight="bold"
-          pb={3}
+    <Box maxWidth="100vw" overflow="hidden">
+      <animated.div style={{ opacity }}>
+        <Box
+          bg="#f0eeeb"
+          p={{ base: 4, md: 8 }}
+          height={{ base: "40vh", md: "auto" }}
+          textAlign="center"
         >
-          GROWTH PARTNERS TO SOME OF TOP D2C BRANDS IN INDIA
-        </Heading>
-        <Text
-          fontSize={{ base: "2xl", md: "5xl" }}
-          mt={-4}
-          pb={2}
-          color="#FFAE34"
-          display="inline-block"
-        >
-          INCLUDING 12 SHARK TANK BRANDS{" "}
-        </Text>
-
-        <Box position="relative" height={slideHeight} overflow="hidden">
-          <Flex
-            width={`${partnerImages.length * 100}%`}
-            transform={`translateX(-${
-              activeIndex * (100 / partnerImages.length)
-            }%)`}
-            transition="transform 0.5s ease-in-out"
+          <Heading
+            mt={5}
+            fontSize={{ base: "xl", md: "3xl" }}
+            fontWeight="bold"
+            pb={3}
           >
-            {partnerImages.map((image, index) => (
-              <Box key={index} flex={`0 0 ${slideWidth}`} boxSize="100%">
-                <Image src={image} alt={`Slide ${index + 1}`} {...imageSize} />
-              </Box>
-            ))}
-          </Flex>
+            GROWTH PARTNERS TO SOME OF TOP D2C BRANDS IN INDIA
+          </Heading>
+          <Text
+            fontSize={{ base: "2xl", md: "5xl" }}
+            mt={-4}
+            pb={2}
+            color="#FFAE34"
+            display="inline-block"
+          >
+            INCLUDING 12 SHARK TANK BRANDS{" "}
+          </Text>
 
-          <Center position="absolute" bottom="10px" width="100%">
-            {partnerImages.map((_, index) => (
-              <Box
-                key={index}
-                boxSize="4"
-                mx={1}
-                bg={index === activeIndex ? "orange.500" : "gray.200"}
-                borderRadius="full"
-                onClick={() => setActiveIndex(index)}
-                cursor="pointer"
-              />
-            ))}
-          </Center>
+          <Box position="relative" height={slideHeight} overflow="hidden">
+            <Flex
+              width={`${partnerImages.length * 100}%`}
+              transform={`translateX(-${
+                activeIndex * (100 / partnerImages.length)
+              }%)`}
+              transition="transform 0.5s ease-in-out"
+            >
+              {partnerImages.map((image, index) => (
+                <Box key={index} flex={`0 0 ${slideWidth}`} boxSize="100%">
+                  <Image
+                    src={image}
+                    alt={`Slide ${index + 1}`}
+                    {...imageSize}
+                  />
+                </Box>
+              ))}
+            </Flex>
+
+            <Center position="absolute" bottom="10px" width="100%">
+              {partnerImages.map((_, index) => (
+                <Box
+                  key={index}
+                  boxSize="4"
+                  mx={1}
+                  bg={index === activeIndex ? "orange.500" : "gray.200"}
+                  borderRadius="full"
+                  onClick={() => setActiveIndex(index)}
+                  cursor="pointer"
+                />
+              ))}
+            </Center>
+          </Box>
         </Box>
-      </Box>
-    </animated.div>
+      </animated.div>
+    </Box>
   );
 };
 
